92b45fe0140b7c13e9801f7d763f6dc2d3e7921a
Nikky Rojas <rojas7313@gmail.com>
2022-06-11 22:19:50 -0600

    encrypt method tests

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -24,19 +24,19 @@ RSpec.describe Enigma do
   end
 
   it "encrypt method has a encrypted string" do
-    expect(@enigma.encrypt.encryption).to eq("jcyfu")
+    expect(@enigma.encrypt("hello world").encryption).to eq(encrypted_string)
   end
 
   it "encrypt method has a key for encrypted string" do
-    expect(@enigma.encrypt.key).to eq("46488")
+    expect(@enigma.encrypt("hello world").key).to eq(key_string)
   end
 
   it "encrypt method has date used for encrypted string in ddmmyy format" do
-    expect(@enigma.encrypt.date).to eq("110622")
+    expect(@enigma.encrypt("hello world").date).to eq(date_string)
   end
 
   it "creates a hash with the encrypt method" do
-    expect(@enigma.encrypt("whatever")).to eq(Hash)
+    expect(@enigma.encrypt("hello world")).to eq(@encrypted)
   end
 
 end
