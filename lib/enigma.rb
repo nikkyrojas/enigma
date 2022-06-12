aa31c6ffd996bed35485676fc83ede90bf409a9c
Nikky Rojas <rojas7313@gmail.com>
2022-06-11 22:21:05 -0600

    add encrypt method make_key method make_date method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,14 +1,26 @@
 class Enigma
-  attr_reader e
+  attr_reader :encrypted, :decrypted
 
-def encrypt(message, key*, date*)
-  ({
-    :message  => "Hello",
-    :key      => ,
-    :date     => ,
-    })
+  def encrypt(message) #key_string = make_key, date_string = make_date)
+    @encrypted = ({
+      encryption: encrypted_string,
+      key:        key_string,
+      date:       date_string
+      })
+    end
+
+
+  def make_key
+    random_numbers = []
+    5.times do
+      random_numbers << rand(1..9)
+      end
+    random_numbers.join
   end
 
-end
+  def make_date
+    Time.now.strftime("%d%m%y")
+  end
+
 
 end
