6bdf048a5767637d8dde2341bc5aa8f7ff7d3dc0
Nikky Rojas <rojas7313@gmail.com>
2022-06-14 08:51:50 -0600

    create decrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,14 +4,14 @@ class Enigma < Ciphen
   def encrypt(message, key = make_key, date = make_date)
     encrypted_msg = encrypt_msg(message, key, date)
     ({
-      encryption: encrypted_msg.join,
+      encryption: encrypted_msg,
       key:        key,
       date:       date
       })
   end
 
   def decrypt(message, key, date)
-     # decrypted_msg = Decrypt.decrypted_msg
+     decrypted_msg = decrypt_msg(message, key, date)
      ({
       decryption: decrypted_msg,
       key:        key,
